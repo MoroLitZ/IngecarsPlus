2a94209f037c87e987f218313f8b71261bfeda7a
MoroLitZ <109384225+MoroLitZ@users.noreply.github.com>
2026-04-27 09:04:23 -0500

    Add files via upload

---
+++ proyecto_funcional/app/controller/consultas/ConsultasController.php
@@ -0,0 +1,12 @@
+<?php
+
+use ConsultasController as GlobalConsultasController;
+
+require_once(PATH_MODELS .'consultas/ConsultasModel.php');
+
+class ConsultasController extends Controller {
+    public function exec(){
+        echo "kjuhgjkgkghik";
+        $this->render(__CLASS__);
+    }
+}
---
+++ proyecto_funcional/app/views/Register/registro.php
@@ -0,0 +1,18 @@
+<link rel="stylesheet" type= "text/css" href="../css/css_login.css">
+<div class="wrapper">
+    <form class="login">
+      <p class="title">Registro</p>
+      <input type="text" placeholder="Username" autofocus/>
+      <i class="fa fa-user"></i>
+      <input type="password" placeholder="Password" />
+      <input type="password" placeholder="Confirm password" />
+      <i class="fa fa-key"></i>
+      <a href="../Login/login.php">¿Ya tienes cuenta?</a>
+      <button>
+        <i class="spinner"></i>
+        <span class="state">Registrarte</span>
+      </button>
+    </form>
+    <footer><a target="blank" href="../Home/homepage.php">Ingecarplus.com</a></footer>
+    </p>
+  </div>
